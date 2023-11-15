f14359cc0b7fffd241abc4e93becd148a4719898
Erik Schneider <erik.schneider.pacheco@gmail.com>
2023-11-15 15:18:45 -0300

    Header style finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import { Link, Outlet } from "react-router-dom";
 
 function Header() {
   return (
-    <div>
-      <nav className="list-none">
-        <li className="">
+    <div className="">
+      <nav className="list-none flex p-8 gap-4 w-full justify-center items-center">
+        <li className="p-4 bg-blue-400 text-white rounded m-0">
           <Link to={"/"}>Home</Link>
         </li>
-        <li>
+        <li className="p-4 bg-blue-400 text-white rounded m-0">
           <Link to={"/Chat"}>Chat</Link>
         </li>
       </nav>
